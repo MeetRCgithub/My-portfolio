8058cbb10e327567864084eee93762cf56958f04
Meetgithubac <hellomotorola132@gmail.com>
2024-05-23 20:48:27 +0530

    about and portfolio page added

---
+++ scripts/about.js
@@ -0,0 +1,29 @@
+// let container1 = document.querySelector("#edubt");
+// let container2 = document.querySelector("#expbt");
+
+// container2.addEventListener('click',function() {
+//     let box = document.querySelector("#edubtn");
+//     box.style.display="none";
+//     let box1 = document.querySelector("#expbtn");
+//     box1.style.display="inline-block";
+// })
+
+// container1.addEventListener('click',function() {
+//     let box1 = document.querySelector("#expbtn");
+//     box.style.display="none";
+//     let box = document.querySelector("#edubtn");
+//     box1.style.display="inline-block";
+// })
+
+function educationbtn(){
+    let box1 = document.querySelector("#expbtn");
+    let box = document.querySelector("#edubtn");
+    box1.style.display="none";
+    box.style.display="inline-block";
+}
+function experiencebtn(){
+    let box1 = document.querySelector("#edubtn");
+    let box = document.querySelector("#expbtn");
+    box1.style.display="none";
+    box.style.display="inline-block";
+}
